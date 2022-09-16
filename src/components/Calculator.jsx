ea72fe62cc85b5f5992ad161aef2106c7576dbc9
Filipe Bacof <filipebacof@gmail.com>
2022-09-16 01:04:07 -0300

    Projeto Finalido + Bug Encontrado

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,6 +5,8 @@ import { Box } from '@mui/system';
 
 export default function Calculator() {
     const [num, setNum] = useState(0);
+    const [oldNum, setOldNum] = useState(0);
+    const [operator, setOperator] = useState();
     
     function inputNum (e) {
         let input = e.target.value
@@ -31,8 +33,23 @@ export default function Calculator() {
         }
     }
 
+    function operatorHandler(e) {
+        let operatorInput = e.target.value;
+        setOperator(operatorInput);
+        setOldNum(num);
+        setNum(0);
+    }
+
     function calculate() {
-        alert('calculou');
+        if (operator === "/") {
+            setNum(parseFloat(oldNum) / parseFloat(num));
+        } else if (operator === "X") {
+            setNum(parseFloat(oldNum) * parseFloat(num));
+        } else if (operator === "-") {
+            setNum(parseFloat(oldNum) - parseFloat(num));
+        }else if (operator === "+") {
+            setNum(parseFloat(oldNum) + parseFloat(num));
+        }
     }
     
     return (
@@ -44,23 +61,24 @@ export default function Calculator() {
                     <h1 className='resultado'>{num}</h1>
                     <button onClick={clear}>AC</button>
                     <button onClick={changeSign}>+/-</button>
+                    {/* tem um bug com o botão de porcentagem, pois é exibido um numero muito grande e não cabe na calculadora */}
                     <button onClick={porcentagem}>%</button>
-                    <button className='orange'>/</button>
+                    <button className='orange' onClick={operatorHandler} value={'/'}>/</button>
                     <button className='grey' onClick={inputNum} value={7}>7</button>
                     <button className='grey' onClick={inputNum} value={8}>8</button>
                     <button className='grey' onClick={inputNum} value={9}>9</button>
-                    <button className='orange'>X</button>
+                    <button className='orange' onClick={operatorHandler} value={'X'}>X</button>
                     <button className='grey' onClick={inputNum} value={4}>4</button>
                     <button className='grey' onClick={inputNum} value={5}>5</button>
                     <button className='grey' onClick={inputNum} value={6}>6</button>
-                    <button className='orange'>-</button>
+                    <button className='orange' onClick={operatorHandler} value={'-'}>-</button>
                     <button className='grey' onClick={inputNum} value={1}>1</button>
                     <button className='grey' onClick={inputNum} value={2}>2</button>
                     <button className='grey' onClick={inputNum} value={3}>3</button>
-                    <button className='orange'>+</button>
+                    <button className='orange' onClick={operatorHandler} value={'+'}>+</button>
                     <button className='grey' onClick={inputNum} value={0}>0</button>
                     <button style={{visibility: "hidden"}}>k</button> {/* Esse componente serve apenas para preencher um espaço em branco e ficar escondido, não delete ele */}
-                    <button className='grey' onClick={inputNum} value={","}>,</button>
+                    <button className='grey' onClick={inputNum} value={"."}>,</button>
                     <button className='orange' onClick={calculate}>=</button>
                 </div>
             </Container>
